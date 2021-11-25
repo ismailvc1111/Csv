95c2c2a4c228b619440da5d3bbb3e3fe167b442c
rayoismail <65721696+ismailvc1111@users.noreply.github.com>
2021-11-25 16:31:20 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+import java.security.AccessController;
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+ 
+}
+}
---
+++ ejerci1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+import java.security.AccessController;
+
+public class ejerci1 {
+String ruta;
+
+public ejerci1(String ruta){
+this.ruta = ruta;
+}
+
+
+}
