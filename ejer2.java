dea78a85da03922de4c2e0560426377bff5e5360
rayo ismail <65721696+ismailvc1111@users.noreply.github.com>
2021-11-25 17:49:43 +0000

    pdf

---
+++ ejer2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+import java.security.AccessController;
+import java.io.FileFilter;
+import java.text.SimpleDateFormat;
+public class ejer2 {
+String ruta 
+public ejer2{
+
+
+
+
+}
+
+
+
+
+}
